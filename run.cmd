a5f473fde82425bc79cfe80fe6fa5fa39ae2825f
sergiokapone <sergiy.ponomarenko@gmail.com>
2026-05-05 19:28:18 +0300

    add rm extensions

--- run.cmd
+++ run.cmd
@@ -1,2 +1,4 @@
+
 latexmk -f -g ClassicalElectrodynamics.tex
-latexmk -c ClassicalElectrodynamics.tex
+::latexmk -c ClassicalElectrodynamics.tex
+rm -f ClassicalElectrodynamics.{aux,bbl,blg,bcf,idx,ind,ilg,log,lof,lot,out,toc,acn,acr,alg,glg,glo,gls,fls,fdb_latexmk,snm,nav,vrb,xdv,synctex.gz,run.xml,thm}
