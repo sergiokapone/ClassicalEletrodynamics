1abaa206a5f9f728e0b7ead3f70e7bb939cda1a9
sergiokapone <sergiy.ponomarenko@gmail.com>
2026-05-09 09:27:01 +0300

    refactor cleaning script

--- rm_job.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-rm -f ClassicalElectrodynamics.{aux,bbl,blg,bcf,idx,ind,ilg,log,lof,lot,out,toc,acn,acr,alg,glg,glo,gls,fls,fdb_latexmk,snm,nav,vrb,xdv,synctex.gz,run.xml,thm,fdb_latexmk}
